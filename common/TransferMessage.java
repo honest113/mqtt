831db6a23dbcd6c14785876672baf6dd17faf9c0
honest113 <httstartteh@gmail.com>
2021-12-14 23:20:25 +0700

    subscriber support json transfer msg

--- common/TransferMessage.java
+++ common/TransferMessage.java
@@ -14,6 +14,40 @@ public class TransferMessage {
     private Topic topic;
     private String datetime;
 
+    /** getter - setter */
+
+    public void setMessage(String _message) {
+        this.message = _message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setSourceName(String _srcName) {
+        this.sourceName = _srcName;
+    }
+
+    public String getSourceName() {
+        return this.sourceName;
+    }
+
+    public void setTopic(Topic _topic) {
+        this.topic = _topic;
+    }
+
+    public Topic getTopic() {
+        return this.topic;
+    }
+
+    public void setDateTime(String _datetime) {
+        this.datetime = _datetime;
+    }
+
+    public String getDateTime() {
+        return this.datetime;
+    }
+
     public TransferMessage(String _message, String _srcName, Topic _topic) {
         this.message = _message;
         this.sourceName = _srcName;
@@ -21,6 +55,15 @@ public class TransferMessage {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYYY/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         this.datetime = dtf.format(now);
+    }
+
+    public TransferMessage(String _json, boolean _isJson) {
+        Gson gson = new Gson();
+        TransferMessage transferMessage = gson.fromJson(_json, TransferMessage.class);
+        this.message = transferMessage.message;
+        this.sourceName = transferMessage.sourceName;
+        this.topic = transferMessage.topic;
+        this.datetime = transferMessage.datetime;
     }
 
     @Override
